826f766aa9bef190235b973102c56b2787280399
Marcosky1 <144145890+Marcosky1@users.noreply.github.com>
2024-05-03 11:32:17 -0500

    Mejora de Contacto

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,12 +1,24 @@
-function Contact(){
-    return(
-        <>
-           <h5>Informacion de Contacto</h5>
-           <p>Correo: marco.cortez@tecsup.edu.pe</p>
-           <p>Telefono: 964827634</p>
-           <p>Direccion: San Juan de Lurigancho - Lima</p>
-        </>
-    )
+
+
+function Contact() {
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-8 offset-md-2">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title text-center mb-4">Información de Contacto</h5>
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item"><strong>Correo:</strong> marco.cortez@tecsup.edu.pe</li>
+                                <li className="list-group-item"><strong>Teléfono:</strong> 964827634</li>
+                                <li className="list-group-item"><strong>Dirección:</strong> San Juan de Lurigancho - Lima</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Contact
+export default Contact;
